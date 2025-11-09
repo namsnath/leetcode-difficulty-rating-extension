8b071a9e89afcd2ae9c59ad6082bd7763b33a680
Namit Nathwani <namsnath@gmail.com>
2025-11-09 23:58:46 +0530

    fix: ignores unneeded files in .xpi

---
+++ web-ext-config.mjs
@@ -0,0 +1,10 @@
+export default {
+    ignoreFiles: [
+        'build/**',
+        'images/**',
+        'README.md',
+        'Makefile',
+        '.gitignore',
+        'web-ext-config.mjs',
+    ],
+};
